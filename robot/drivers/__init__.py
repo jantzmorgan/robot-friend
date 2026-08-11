d27389f38716bb109d3722f294cdd93e64dc706c
jantzmorgan <jantzswag@gmail.com>
2026-08-11 18:46:36 -0500

    Enable real Windows speech and mouth animation

--- robot/drivers/__init__.py
+++ robot/drivers/__init__.py
@@ -5,13 +5,13 @@ from .simulated import (
     SimulatedDisplay,
     SimulatedMotion,
     SimulatedSensors,
-    KokoroAudioOutput,
+    KokoroAudioOutput, OpenAIAudioOutput,
     CameraAwareSensors,
 )
 from .opencv_camera import OpenCVCamera
 
 __all__ = [
     "SimulatedAudioInput", "SimulatedAudioOutput", "SimulatedCamera",
-    "SimulatedDisplay", "SimulatedMotion", "SimulatedSensors", "KokoroAudioOutput",
+    "SimulatedDisplay", "SimulatedMotion", "SimulatedSensors", "KokoroAudioOutput", "OpenAIAudioOutput",
     "CameraAwareSensors", "OpenCVCamera",
 ]
